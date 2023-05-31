17ad2e7c5df228cc62d281a2918927b6ac199ad2
William Zale <williamzale@gmail.com>
2023-05-30 22:29:25 -0400

    refactor: Remove comment

--- lib/author.rb
+++ lib/author.rb
@@ -11,8 +11,6 @@ class Author
     @first_name + " " + @last_name
   end
 
-  {author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"}
-
   def write(title, date)
     book_hash = {}
     book_hash[:author_first_name] = @first_name 
