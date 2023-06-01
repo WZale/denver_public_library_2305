8286d46d651f58caf3c719e71fcaca2713e6e24d
William Zale <williamzale@gmail.com>
2023-05-31 22:53:23 -0400

    feat: Add publication_time_frame method

--- lib/library.rb
+++ lib/library.rb
@@ -14,4 +14,15 @@ class Library
     @books << author.books
     @books = @books.flatten
   end
+
+  def publication_time_frame(author)
+    time_frame_hash = {}
+    years = author.books.map do |book|
+      book.publication_year.to_i
+    end
+    time_frame_hash[:start] = years.min.to_s
+    time_frame_hash[:end] = years.max.to_s
+    time_frame_hash
+  end
+
 end
